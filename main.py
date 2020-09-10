b0a2327c382c91ad09ee45ef53c8263c7e85baf4
VodoGamer <54740235+VodoGamer@users.noreply.github.com>
2020-09-10 07:17:01 +0300

    дадая доебался до импортов

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import random
-import time
+from random import randint
+from time import sleep
 
 print("[-] Генерируем силы...")
-mHitStrn = random.randint(3, 40)
-mHitCrit = random.randint(12, 79)
-mWSpeed = random.randint(1, 30)
+mHitStrn = randint(3, 40)
+mHitCrit = randint(12, 79)
+mWSpeed = randint(1, 30)
 mHP = 100
 
-aHitStrn = random.randint(3, 40)
-aHitCrit = random.randint(12, 79)
-aWSpeed = random.randint(1, 30)
+aHitStrn = randint(3, 40)
+aHitCrit = randint(12, 79)
+aWSpeed = randint(1, 30)
 aHP = 99
 
 print("[-] Началась битва!")
@@ -17,18 +17,18 @@ print(f"[-] Статистика Мамикса:\n - Сила удара: {mHitS
 print("[-] Подождите...")
 
 while True:
-    time.sleep(1)
+    sleep(1)
     if mHP >= 1 and aHP >= 1:
-            if random.randint(1, 500) >= 250:
-                if random.randint(1, 500) >= 420:
+            if randint(1, 500) >= 250:
+                if randint(1, 500) >= 420:
                     print("[-] АНАЛ 4  снес МАМЕГСУ " + str(aHitCrit / 2) + "! Новое хп : " + str(mHP))
                     mHP = mHP - aHitCrit / 2
                 else:
                     print("[-] АНАЛ 4  снес МАМЕГСУ " + str(aHitStrn / 2) + "! Новое хп : " + str(mHP))
                     mHP = mHP - aHitStrn / 2
 
-            if random.randint(1, 500) >= 250:
-                if random.randint(1, 500) >= 420:
+            if randint(1, 500) >= 250:
+                if randint(1, 500) >= 420:
                     print("[-] МАМЕГС  снес АНАЛУ 4 " + str(mHitCrit / 2) + "! Новое хп : " + str(aHP))
                     aHP = aHP - mHitCrit / 2
                 else:
